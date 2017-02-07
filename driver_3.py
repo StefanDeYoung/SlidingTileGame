47c02bdd798eee3e295256cdc30bfb2b42426572
Stefan <stefandeyoung@gmail.com>
2017-02-07 16:35:24 -0500

    switched to node/solver class system in main. Added dummy functions for dfs,ast, ida

--- driver_3.py
+++ driver_3.py
@@ -7,36 +7,31 @@ import BoardSearch as bs
 def main(argv):
     '''
     Program is called from the command line like this:
-            $ python driver.py <method> <boardLayout> 
+            $ python driver.py <method> <boardLayout>
             where,
             <method> is one of bfs, dfs, ast, ida
             <boardLayout> is a comma separated list representing a nXn board
-    '''            
-    print ('Number of arguments: ', len(argv))
-    print ('Argument list: ', str(argv))
+    '''
+    #print ('Number of arguments: ', len(argv))
+    #print ('Argument list: ', str(argv))
 
     searchType = argv[1]    # get search type requested (ie. bfs, dfs, ast, or ida)
-    
-    # change argv[2] from string '0,1,2,...,8' to a list ['0','1','2',...,'8']
-    initState = argv[2].split(',')              # split the string into a list of single chars
-    # convert list of strings ['0','1','2',...,'8'] to list of int [0,1,2,...,8]
-    initState = [int(x) for x in initState]
+
+    # change argv[2] from string '0,1,2,...,8' to a list ['0','1','2',...,'8'] then to a list of int [0,1,2,...,8]
+    root = new State    #STEFAN!
+    root.state = argv[2].split(',')
+    root.state = [int(x) for x in root.state]
+    root.state = tuple(root.state)
 
     # Calculate board dimension (n X n)
-    n = int(math.sqrt(len(initState)))
+    n = len(root.state)
 
-    # convert initState to a tuple (0,1,2,...,n-1)
-    initState = tuple(initState)  
+    # generate the goal state as a tuple --> (0,1,2,...,n-1)
+    goal.state = tuple(range(n))
 
-    # generate the goal state as a tuple --> (0,1,2,...,n-1) 
-    goalState = tuple(range(len(initState)))
-    
-    print ('searchType: ', searchType)
-    print ('initState: ', initState)
-    print ('Goal State:', goalState)
-    print ('Board dimension:', n)
+    readout = solver (root, goal, searchType)
 
-    problem = bs.BoardSearchProblem(initState, goalState, n)
+    prepareOutput (readout)
 
     # prepare to measure execution time of search function
     t_Start = time.time()
@@ -47,32 +42,41 @@ def main(argv):
     print ('Pushes:',pushes,', Pops:',pops,', Fringe @ goal:',f_size,', Fringe max size:',f_maxsize,', expanded:',expanded )
     print ('Depth @ goal:', finish.depth, ', maxDepth:', maxDepth)
 
+def prepareOutput(readout):
     ### Print output to file output.txt
     orig_stdout = sys.stdout
     f = open('output.txt', 'w')
     sys.stdout = f
 
-    print ('path_to_goal:',finish.path)
+    print ('path_to_goal:',readout.finish.path)
     print ('cost_of_path:')
-    print ('nodes_expanded:', expanded)
-    print ('fringe_size:', f_size)
-    print ('max_fringe_size:', f_maxsize)
-    print ('search_depth:', finish.depth)
-    print ('max_search_depth:', maxDepth)
+    print ('nodes_expanded:', readout.expanded)
+    print ('fringe_size:', readout.f_size)
+    print ('max_fringe_size:', readout.f_maxsize)
+    print ('search_depth:', readout.finish.depth)
+    print ('max_search_depth:', readout.maxDepth)
     print ('running_time:')
-    print ('max_ram_usage:')   
+    print ('max_ram_usage:')
 
     sys.stdout = orig_stdout
     f.close()
-    #End of printout 
-    
-def bfs(problem):
-    """Search the shallowest nodes in the search tree first."""
+    #End of printout
 
-    startNode = problem.getStartState()
-    print ('** B F S   **************************************')
-    #print ('StartNode.state:(', startNode.stateID,'), Parent:(',startNode.parentID,'), Path:(',startNode.path,')')
-    #print ('Goal:', problem.goalState)
+def solver(root, goal, searchType)
+
+    if (searchType == 'bfs'):
+        bfs(root, goal)
+    elif (searchType == 'dfs'):
+        dfs(root, goal)
+    elif (searchType == 'ast'):
+        ast(root, goal)
+    elif (searchType == 'ida'):
+        ida(root, goal)
+    else:
+        print("Invalid searchType")
+
+def bfs(root, goal):
+    """Search the shallowest nodes in the search tree first."""
 
     '''
     for node in problem.getSuccessors(startNode):
@@ -84,7 +88,7 @@ def bfs(problem):
     f_maxsize = 0
     f_pops = 0
     f_pushes = 0
-    
+
     y = bs.Queue()
     visitedNodes = set()
     y.push(startNode)
@@ -110,11 +114,20 @@ def bfs(problem):
                     if f_currentSize > f_maxsize:
                         f_maxsize = f_currentSize
         #else:
-            #print ('%%%%% node: ', node.stateID, ' alredy in "visited"')                
+            #print ('%%%%% node: ', node.stateID, ' alredy in "visited"')
+
+def dfs(root, goal):
+    print('dfs is not yet implemented')
+
+def ast(root, goal):
+    print('ast is not yet implemented')
+
+def ida(root, goal):
+    print('ida is not yet implemented')
+
 
 # -------------------------------------------------------
 ''' This is where the program starts and retrieves the command line args'''
 if __name__ == '__main__':  # python calls it's main program "__main__"
     main(sys.argv)          # sys.argv returns the command line string
 # -------------------------------------------------------
-    
